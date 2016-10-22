652fb5abe2011ef7a831860d0882b3e1a2088adf
lazamar <lazaronijunior@gmail.com>
2016-10-22 22:35:40 +0100

    Parse video types

--- src/elm/Tweets/TweetParser.elm
+++ src/elm/Tweets/TweetParser.elm
@@ -48,16 +48,17 @@ type ExtendedMedia
 
 
 type alias ExtendedPhotoRecord =
-    { url: String -- what is in the tweet
-    , display_url: String -- what should be shown in the tweet
+    { url : String -- what is in the tweet
+    , display_url : String -- what should be shown in the tweet
     , media_url_https : String -- the actuall address of the content
     }
 
 
 
 type alias ExtendedVideoRecord =
-    { url: String
-    , variants: List VariantRecord
+    { url : String
+    , display_url : String
+    , variants : List VariantRecord
     }
 
 
@@ -154,10 +155,6 @@ urlDecoder =
 
 
 
--- EXTENDED ENTITIES
-
-
-
 extendedEntitiesDecoder : Decoder ExtendedEntitiesRecord
 extendedEntitiesDecoder =
     decode ExtendedEntitiesRecord
@@ -188,6 +185,7 @@ extendedVideoRecordDecoder : Decoder ExtendedVideoRecord
 extendedVideoRecordDecoder =
     decode ExtendedVideoRecord
         |> required "url" string
+        |> required "display_url" string
         |> requiredAt ["video_info", "variants"] ( list variantRecordDecoder )
 
 
@@ -206,3 +204,128 @@ variantRecordDecoder =
     decode VariantRecord
         |> required "content_type" string
         |> required "url" string
+
+
+
+-- PROCESSING
+
+
+
+type MediaRecord2
+    = MultiPhotoMedia MultiPhoto
+    | VideoMedia Video
+
+
+
+type alias MultiPhoto =
+    { url : String -- what is in the tweet
+    , display_url : String -- what should be shown in the tweet
+    , media_url_list : List String -- the actuall addresses of the contents
+    }
+
+
+
+type alias Video =
+    { url: String -- what is in the tweet
+    , display_url:  String -- what should be shown in the tweet
+    , content_type : String
+    , url : String -- the actuall addresses of the contents
+    }
+
+
+
+mergeMediaLists : List ExtendedMedia -> List MediaRecord2 -> List MediaRecord2
+mergeMediaLists extendedMedia media =
+    let
+        photos = getPhotos extendedMedia
+        videos = getVideos extendedMedia
+    in
+        List.concat [photos, videos]
+
+
+
+getPhotos : List ExtendedMedia -> List MediaRecord2
+getPhotos extendedMedia =
+    extendedMedia
+        |> List.filterMap toExtendedPhoto
+        |> groupByUrl
+        |> List.filterMap
+            (\group ->
+                List.foldr
+                    (\extendedPhoto maybeMediaType ->
+                        Just <| case maybeMediaType of
+                            Nothing ->
+                                MultiPhoto
+                                    extendedPhoto.url
+                                    extendedPhoto.display_url
+                                    [ extendedPhoto.media_url_https ]
+
+                            Just mediaType ->
+                                { mediaType
+                                | media_url_list =
+                                    extendedPhoto.media_url_https :: mediaType.media_url_list
+                                }
+                    )
+                    Nothing
+                    group
+            )
+        |> List.map MultiPhotoMedia
+
+
+
+toExtendedPhoto : ExtendedMedia -> Maybe ExtendedPhotoRecord
+toExtendedPhoto extendedMedia =
+    case extendedMedia of
+        ExtendedPhoto extendedPhoto ->
+            Just extendedPhoto
+
+        otherwise ->
+            Nothing
+
+
+
+groupByUrl : List ExtendedPhotoRecord -> List (List ExtendedPhotoRecord)
+groupByUrl mediaList =
+    mediaList
+        |> List.foldr
+            (\m uniqueUrls -> if List.member m.url uniqueUrls then uniqueUrls else m.url :: uniqueUrls )
+            []
+        |> List.map
+            (\url ->
+                List.filter (\mediaItem -> mediaItem.url == url) mediaList
+            )
+
+
+
+getVideos : List ExtendedMedia -> List MediaRecord2
+getVideos extendedMedia =
+    extendedMedia
+        |> List.filterMap toExtendedVideo
+        |> List.map extendedVideoToVideo
+        |> List.map VideoMedia
+
+
+
+toExtendedVideo : ExtendedMedia -> Maybe ExtendedVideoRecord
+toExtendedVideo extendedMedia =
+    case extendedMedia of
+        ExtendedVideo extendedPhoto ->
+            Just extendedPhoto
+
+        otherwise ->
+            Nothing
+
+
+
+extendedVideoToVideo : ExtendedVideoRecord -> Video
+extendedVideoToVideo extendedVideo =
+    let
+        videoVariant =
+            List.head extendedVideo.variants
+                |> Maybe.withDefault ( VariantRecord "nothingHere" "nothingHere" )
+    in
+        Video
+            extendedVideo.url
+            extendedVideo.display_url
+            videoVariant.content_type
+            videoVariant.url
