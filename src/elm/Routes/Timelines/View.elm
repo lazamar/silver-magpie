1f74e647b9dcd65ae4384108a5f71bdaf40fb15c
lazamar <lazaronijunior@gmail.com>
2016-11-19 21:06:28 +0000

    Fix layout after refactoring

--- src/elm/Routes/Timelines/View.elm
+++ src/elm/Routes/Timelines/View.elm
@@ -4,13 +4,14 @@ import Routes.Timelines.Types exposing (..)
 import Routes.Timelines.TweetBar.View
 import Routes.Timelines.Timeline.View
 import Html exposing ( Html, div )
+import Html.Attributes exposing ( class )
 import Html.App
 
 
 
 root : Model -> Html Msg
 root model =
-    div []
+    div [ class "Timelines" ]
         [ Routes.Timelines.Timeline.View.root model.timelineModel
             |> Html.App.map TimelineMsgLocal
 
