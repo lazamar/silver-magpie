25d6f03ec72b79b1538a5467210d11710f75fd5b
lazamar <lazaronijunior@gmail.com>
2016-11-27 23:05:42 +0000

    Add thumbnail 128x128

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 /* eslint-disable quote-props */
+
+// Resize image:
+// convert    logo.png   -thumbnail '128x128>'   -background transparent   -gravity center   -extent 128x128   -compose Copy_Opacity      original-resized.png
+
 // List all available tasks
 const organiser = require('gulp-organiser');
 organiser.registerAll('./tasks', {
