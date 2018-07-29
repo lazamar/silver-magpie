119f741bd6bc38305223bef1687257929332adb1
Marcelo Lazaroni <lazaronijunior@gmail.com>
2018-07-29 21:56:05 +0100

    Create version bumping task that just bumps js file versions

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,38 @@
 // convert    logo.png   -thumbnail '128x128>'   -background transparent   -gravity center   -extent 128x128   -compose Copy_Opacity      original-resized.png
 
 // List all available tasks
-const organiser = require('gulp-organiser');
-organiser.registerAll('./tasks', {
-  sass: {
-    src: 'src/styles/**/*.scss',
-    dest: 'dist/styles',
-  },
-  'copy-static': {
-    src: ['src/**/*', '!src/styles/**/*', '!**/*.elm'],
-    dest: 'dist',
-  },
-  'build-elm': {
-    watch: 'src/elm/**/*',
-    src: 'src/elm/Main.elm',
-    dest: 'dist/elm',
-    moduleName: 'Main',
-    ext: 'js',
-  },
-  'browser-sync': {
-    src: '.', // it doesn't matter, it's just so the task object is not ignored.
-    reloadOn: ['build-elm', 'copy-static', 'sass'], // reload page when these tasks happen
-    startPath: 'dist/pages/index.html',
-    baseDir: './',
-  },
-  'create-zip': {
-    src: './dist',
-  },
-  'build': {
-    src: './undefined',
-    tasks: ['copy-static', 'sass', 'build-elm', 'create-zip'],
-  },
+const organiser = require("gulp-organiser");
+organiser.registerAll("./tasks", {
+    sass: {
+        src: "src/styles/**/*.scss",
+        dest: "dist/styles"
+    },
+    "copy-static": {
+        src: ["src/**/*", "!src/styles/**/*", "!**/*.elm"],
+        dest: "dist"
+    },
+    "build-elm": {
+        watch: "src/elm/**/*",
+        src: "src/elm/Main.elm",
+        dest: "dist/elm",
+        moduleName: "Main",
+        ext: "js"
+    },
+    "browser-sync": {
+        src: ".", // it doesn't matter, it's just so the task object is not ignored.
+        reloadOn: ["build-elm", "copy-static", "sass"], // reload page when these tasks happen
+        startPath: "dist/pages/index.html",
+        baseDir: "./"
+    },
+    "create-zip": {
+        src: "./dist"
+    },
+    build: {
+        src: "./undefined",
+        tasks: ["copy-static", "sass", "build-elm", "create-zip"]
+    },
+    "bump-version": {
+        src: ["./package.json"],
+        bumpType: "patch"
+    }
 });
---
+++ tasks/bump-version.js
@@ -0,0 +1,79 @@
+const gulp = require("gulp");
+const organiser = require("gulp-organiser");
+const Future = require("fluture");
+const fs = require("fs");
+const assert = require("assert");
+const childProcess = require("child_process");
+
+////////////////////////////////////////////////////////////////////////
+// Configuration:
+const exampleConfiguration = { // eslint-disable-line
+    files: ["./package.json", "src/manifest.json"],
+    bumpType: "patch" // "minor" and "major" also accepted.
+};
+////////////////////////////////////////////////////////////////////////
+
+// ====== FILE SYSTEM =====
+
+const readFile = address =>
+    Future((reject, resolve) => {
+        fs.readFile(address, "utf8", (err, data) => (err ? reject(err) : resolve(data)));
+    });
+
+const writeFile = address => data =>
+    Future((reject, resolve) => {
+        fs.writeFile(address, data, "utf8", err => (err ? reject(err) : resolve()));
+    });
+
+const readJSON = address => readFile(address).map(JSON.parse);
+
+// Run shell command
+const exec = command =>
+    Future((reject, resolve) => {
+        childProcess.exec(command, (err, stdout) => (err ? reject(err) : resolve(stdout)));
+    });
+
+const MAJOR = "major";
+const MINOR = "minor";
+const PATCH = "patch";
+
+const parseNum = n => parseInt(n, 10) || 0;
+
+// Will bump a string version according to a bump type
+// MAJOR, 1.1.1 -> 2.1.1
+// MINOR, 1.1.1 -> 1.2.1
+// PATCH, 1.1.1 -> 1.1.2
+const bumpVersion = bump => aVersion => {
+    // Leave only numbers with dots.
+    // v3.4.5 --> 3.4.5
+    const withoutLetters = aVersion.replace(/[^0-9.]/gi, "");
+    const numbers = withoutLetters.split(".");
+    const major = parseNum(numbers[0]);
+    const minor = parseNum(numbers[1]);
+    const patch = parseNum(numbers[2]);
+
+    const majorBump = bump === MAJOR ? 1 : 0;
+    const minorBump = bump === MINOR ? 1 : 0;
+    const patchBump = bump === PATCH ? 1 : 0;
+
+    assert([MAJOR, MINOR, PATCH].includes(bump), "Invalid bump type: " + bump);
+
+    return [major + majorBump, minor + minorBump, patch + patchBump].join(".");
+};
+
+const setFileVersion = version => address =>
+    readJSON(address)
+        .map(obj => Object.assign({}, obj, { version }))
+        .map(v => JSON.stringify(v, null, 4))
+        .chain(writeFile(address));
+
+module.exports = organiser.register(task => {
+    gulp.task(task.name, done => {
+        // Get current tag, which should be the software version
+        exec("git describe --tags")
+            .map(bumpVersion(task.bumpType))
+            .chain(tag => Future.parallel(Infinity, task.src.map(setFileVersion(tag))))
+            // Trigger things
+            .fork(done, () => done());
+    });
+});
