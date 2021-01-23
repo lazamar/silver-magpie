aa56557c595d042a91cdbc43ee1e378c41588191
Marcelo Lazaroni <lazaronijunior@gmail.com>
2021-01-23 18:16:21 +0000

    Make building not fail if directory already exists

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,3 +1,3 @@
-mkdir dist || echo "dist already exists";
+mkdir -p dist || echo "dist already exists";
 docker build --tag silver-magpie-extension .
 docker run -v $(pwd):/mnt silver-magpie-extension cp dist/build.zip /mnt/dist/build.zip
