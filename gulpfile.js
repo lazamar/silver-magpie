2142a467c69fba618cd43d62991098627dbdc10e
Marcelo Lazaroni <lazaronijunior@gmail.com>
2021-01-24 02:53:48 +0000

    Rename elm.json in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ organiser.registerAll("./tasks", {
         tasks: ["copy-static", "sass", "build-elm", "create-zip"]
     },
     "bump-version": {
-        src: ["./package.json", "./elm-package.json", "./src/manifest.json"],
+        src: ["./package.json", "./elm.json", "./src/manifest.json"],
         bumpType: "patch"
     }
 });
